4ad8d7d7ba1ba299f22e1e310fb7625a135137b9
Nick Babcock <nbabcock19@hotmail.com>
2019-01-09 07:00:59 -0600

    Travis debian packaging

--- ci/before_deploy.sh
+++ ci/before_deploy.sh
@@ -18,12 +18,16 @@ main() {
 
     test -f Cargo.lock || cargo generate-lockfile
 
-    cross rustc --bin dness --target $TARGET --release -- -C lto
+    if [[ -n "$TARGET"  ]]; then
+        cross rustc --bin dness --target $TARGET --release -- -C lto
+        cp target/$TARGET/release/dness $stage/
+        cd $stage
+        tar czf $src/$CRATE_NAME-$TRAVIS_TAG-$TARGET.tar.gz *
+    else
+        docker run -ti --rm -v "$(pwd):/source" rust /bin/bash -c "cd /source && cargo install cargo-deb && cargo deb"
+        cp target/debian/dness*.deb $src/.
+    fi
 
-    cp target/$TARGET/release/dness $stage/
-
-    cd $stage
-    tar czf $src/$CRATE_NAME-$TRAVIS_TAG-$TARGET.tar.gz *
     cd $src
 
     rm -rf $stage
