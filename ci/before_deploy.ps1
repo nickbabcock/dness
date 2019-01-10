41c2194549c4de7461e4c19ad125aec3a1eb666f
Nick Babcock <nbabcock19@hotmail.com>
2019-01-09 21:37:36 -0600

    Fix appveyor push artifact

--- ci/before_deploy.ps1
+++ ci/before_deploy.ps1
@@ -2,18 +2,9 @@
 # release zipfile
 
 $SRC_DIR = $PWD.Path
-$STAGE = [System.Guid]::NewGuid().ToString()
 
-Set-Location $ENV:Temp
-New-Item -Type Directory -Name $STAGE
-Set-Location $STAGE
+Copy-Item "$SRC_DIR\target\$($Env:TARGET)\release\$($Env:CRATE_NAME).exe" "$SRC_DIR\$($Env:CRATE_NAME)-$($Env:APPVEYOR_REPO_TAG_NAME).exe"
 
-Copy-Item "$SRC_DIR\target\$($Env:TARGET)\release\$($Env:CRATE_NAME).exe" "$($Env:CRATE_NAME)-$($Env:APPVEYOR_REPO_TAG_NAME).exe"
+Push-AppveyorArtifact "$SRC_DIR\$($Env:CRATE_NAME)-$($Env:APPVEYOR_REPO_TAG_NAME).exe"
 
-Push-AppveyorArtifact "$($Env:CRATE_NAME)-$($Env:APPVEYOR_REPO_TAG_NAME).exe"
-
-Remove-Item *.* -Force
-Set-Location ..
-Remove-Item $STAGE
 Set-Location $SRC_DIR
-
