50652bc020624c5a2911f5a3b4ef2ee4eff1b9bc
Nick Babcock <nbabcock19@hotmail.com>
2020-01-13 18:14:54 -0600

    Test that musl can be deb-packaged

--- ci/before_deploy.sh
+++ ci/before_deploy.sh
@@ -7,37 +7,44 @@ main() {
     local src=$(pwd) \
           stage=
 
-    case $TRAVIS_OS_NAME in
-        linux)
-            stage=$(mktemp -d)
-            ;;
-        osx)
-            stage=$(mktemp -d -t tmp)
-            ;;
-    esac
+    if [ "$TRAVIS_OS_NAME" = "osx" ]; then
+        stage=$(mktemp -d -t tmp)
+    else
+        stage=$(mktemp -d)
+    fi
 
     test -f Cargo.lock || cargo generate-lockfile
 
+    CARGO_CMD="cargo"
+    CARGO_FLAGS=""
+
     if [ -n "$TARGET" ]; then
-        CARGO_FLAGS=""
-        if [ -n "$RUSTLS" ]; then
-            CARGO_FLAGS="--no-default-features --features rustls"
-        fi
-
-        cross rustc $CARGO_FLAGS --bin dness --target $TARGET --release -- -C lto
-        if [ "$TARGET" = "x86_64-unknown-linux-musl" ]; then
-            cargo install cargo-deb
-            cross deb --target "$TARGET" --variant musl --no-build
-            cp target/"$TARGET"/debian/dness*.deb $src/.
-        fi
-
-        cp target/$TARGET/release/dness $stage/
-        cd $stage
-        tar czf $src/$CRATE_NAME-$TRAVIS_TAG-$TARGET.tar.gz *
-    else
-        docker run -ti --rm -v "$(pwd):/source" rust /bin/bash -c "cd /source && cargo install cargo-deb && cargo deb"
-        cp target/debian/dness*.deb $src/.
+        CARGO_FLAG="$CARGO_FLAGS --target $TARGET"
     fi
+
+    if [ -n "$TARGET" && "$TARGET" != "x86_64-unknown-linux-musl" && "$TARGET" != "x86_64-unknown-linux-gnu" ]; then
+        CARGO_CMD="cross"
+    fi
+
+    if [ -n "$RUSTLS" ]; then
+        CARGO_FLAGS="$CARGO_FLAGS --no-default-features --features rustls"
+    fi
+
+    $CARGO_CMD rustc $CARGO_FLAGS --bin dness --release -- -C lto
+
+    if [ -n "$DEBIAN_PACKAGING" ]; then
+        cargo install cargo-deb
+        VARIANT=""
+        if [ "$TARGET" = "x86_64-unknown-linux-musl" ]; then
+            VARIANT="--variant musl"
+        fi
+        cargo deb $VARIANT --target "$TARGET" --no-build
+        cp target/"$TARGET"/debian/dness*.deb $src/.
+    fi
+
+    cp target/$TARGET/release/dness $stage/
+    cd $stage
+    tar czf $src/$CRATE_NAME-$TRAVIS_TAG-$TARGET.tar.gz *
 
     cd $src
 
